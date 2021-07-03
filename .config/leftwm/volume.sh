656c2c2de13df4f83cf9fe9e0e30e5d730e5f71d
Syudagye <4024427-Syudagye@users.noreply.gitlab.com>
2021-07-03 16:58:01 +0200

    Added dunst notification for volume

--- .config/leftwm/volume.sh
+++ .config/leftwm/volume.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 MAX=100
 vol=$(pulseaudio-ctl full-status)
 newvol=$((${vol:0:3} + $1))
 
 [ $newvol -le $MAX ] && pulseaudio-ctl set $newvol
+
+vol=$(pulseaudio-ctl full-status)
+vol=${vol:0:3}
+
+# Dunst notification
+dunstify -a "changeVolume" -u low -i audio-volume-high -h int:value:"$vol" "Volume: $vol%" -r "69420"
